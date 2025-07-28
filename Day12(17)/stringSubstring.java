68b0ba1fcba991b559f606ce3983237cc0047a9b
sujeth95 <sujeetoraon337@gmail.com>
2025-07-28 21:28:23 +0530

    Learning about Strings Part II

--- Day12(17)/stringSubstring.java
+++ Day12(17)/stringSubstring.java
@@ -1,8 +1,18 @@
 // package Day12(17);
 
 public class stringSubstring {
+
+    public static String subString(String str, int si, int ei){
+        String substr = "";
+        for(int i = si; i<ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
     public static void main(String[] args) {
         //Substring
-        
+        String str = "HelloWorld";
+        // System.out.println(subString(str, 0, 5));
+        System.out.println(str.substring(0,5));
     }
 }
